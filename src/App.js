ac6abb0b15033b15ddb62e3ac71a235adcc3fa8f
Jama82 <88908555+Jama82@users.noreply.github.com>
2022-07-30 15:41:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Todolist from './components/Todolist';
+
+const  App = () => {
+  return (
+      <div className="todo-app">
+
+         
+          <Todolist/>
+      </div>
+   )
+  
+}
+
+export default App;
